955a7b4ebc645ffba5a8c477dd55cd947ef0f9ba
Travis <travismills82@gmail.com>
2020-07-19 22:31:23 -0700

    Shame: unbound energy vortex arn't always a lama

--- Scripts/Services/Dungeons/ShameRevamped/Mobiles/Creatures.cs
+++ Scripts/Services/Dungeons/ShameRevamped/Mobiles/Creatures.cs
@@ -1354,7 +1354,7 @@ namespace Server.Mobiles
         public UnboundEnergyVortex() : base(AIType.AI_Melee, FightMode.Weakest, 10, 1, 0.4, 0.2)
         {
             Name = "an unbound energy vortex";
-            Body = 0xDC;
+            Body = 164;
             Hue = 0x76;
 
             SetStr(450);
